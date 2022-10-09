fc4668f2d8dc302c1305bfe10a23810a579028c5
viliamgago <viliam@flowpay.io>
2022-10-09 11:07:28 +0200

    first files

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="python_world",
+    version="0.0.1",
+    author="Viliam Gago",
+    author_email="vg@s.cz,
+    description="A function that returns 'world'",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/viliamgago/python_world",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
